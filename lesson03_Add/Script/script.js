0c93724cbef72627a8682a28e51a55371b4df70f
Taras Vyshegorodtsev <tasarvv@gmail.com>
2019-10-24 15:52:45 +0300

    первая задача начало

--- lesson03_Add/Script/script.js
+++ lesson03_Add/Script/script.js
@@ -1,7 +1,29 @@
 // bonus01
-let lang = prompt('Р’С‹Р±РµСЂРёС‚Рµ СЏР·С‹Рє РґРЅРµР№ РЅРµРґРµР»Рё: en/ru');
+// Написать программу, которая последовательно запрашивает два числа, 
+// после чего сравнивает их и выводит сообщение: 
+// «Первое число больше второго», «Второе число больше первого» или «Числа равны». 
+// Необходимо учесть ситуации, когда пользователь ввел строку или вообще не ввел ничего.
 
-lang = lang.toLowerCase().trim();
-console.log(lang);
-console.log('lang=ru: ', (lang ==='ru'));
+        let getNumber = function(index){
+            let number;
+            do {
+                number = +prompt('Введите '+ index +'ое число.', index);    
+            } while  (number <= 0 || isNaN(number) || number == null) ;
+            return number;
+        }         
+        let number1 = getNumber(1);
+        let number2 = getNumber (2);
 
+        do {
+            money = +prompt('Введите 1ое число.',1);    
+        } while  (money <= 0 || isNaN(money) || money == null) ;
+
+        Expenses2 = prompt('Введите 2ое число.', 'Кредит');
+        //2) Добавить валидацию (проверку) на данные которые мы получаем на вопрос 
+        //'Во сколько это обойдется?’ в функции  getExpensesMonth
+        do {
+            ExpensesAmount = +prompt('Во сколько это обойдется? ', 7000);
+        } while  (!(ExpensesAmount > 0));
+        
+        sum+=ExpensesAmount;
+    }
