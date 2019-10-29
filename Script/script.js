3a07b560984cb6fcb5d11edfd765e57bb30c6112
Taras Vyshegorodtsev <tasarvv@gmail.com>
2019-10-29 10:57:39 +0300

    сделал 3задачу

--- Script/script.js
+++ Script/script.js
@@ -40,41 +40,45 @@ function checkBeginEnd (year1,year2){
   
 }
 
-// function getNumber (index){
-//     let number;
-//     do {
-//         number = prompt('Введите '+ index +'ое число.', index);    
-//     } while  (isNaN(number) ||  !isFinite(number) || number == null);
-//     return number;
-// }         
-// let number1 = getNumber(1);
-// let number2 = getNumber (2);
-// if (number1 > number2) {
-//     alert('Первое число ' + number1 + ' больше второго ' + number2);
-//     console.log('Первое число ' + number1 + ' больше второго ' + number2);
-// } else if (number1 < number2) {
-//     alert('Второе число ' + number2 + ' больше первого ' + number1);
-//     console.log('Второе число ' + number2 + ' больше первого ' + number1);
-// } else {
-//     alert ('Числа равны');
-//     console.log('Числа равны');
-// }
+function getNumber (index){
+    let number;
+    do {
+        number = prompt('Введите '+ index +'ое число.', index);    
+    } while  (isNaN(number) ||  !isFinite(number) || number == null);
+    return number;
+}
+function task1(){
+  // Написать программу, которая последовательно запрашивает два числа, после чего сравнивает их и выводит сообщение: 
+  // «Первое число больше второго», «Второе число больше первого» или «Числа равны». 
+  // Необходимо учесть ситуации, когда пользователь ввел строку или вообще не ввел ничего.
+  let number1 = getNumber(1);
+  let number2 = getNumber (2);
+  if (number1 > number2) {
+      alert('Первое число ' + number1 + ' больше второго ' + number2);
+      console.log('Первое число ' + number1 + ' больше второго ' + number2);
+  } else if (number1 < number2) {
+      alert('Второе число ' + number2 + ' больше первого ' + number1);
+      console.log('Второе число ' + number2 + ' больше первого ' + number1);
+  } else {
+      alert ('Числа равны');
+      console.log('Числа равны');
+  }
+}
+function task2 (){
 //2
 // Написать программу, которая спрашивает у пользователя начальный год и конечный, после выводит в консоль високосные годы.
 // Алгоритм определения високосного года следует найти в интернете самостоятельно.
 // Если пользователь ввел первый год больше второго, то тот, который меньше, принять за начало отчета, а который больше за конечную точку
 
-// ввод годов 
+//ввод годов 
 let yearBegin = 0, //= prompt("Начальный год", 1970),
     yearEnd = 0, //= prompt("Конечный год", 2070),
     yearTemp = 0;
-
 
 yearBegin = checkNum (yearBegin, "Начальный год", 2070);
 yearEnd = checkNum(yearEnd, "Конечный год", 2070);
 
 //Который год больше/меньше
-// console.log('checkBeginEnd (yearBegin,yearEnd)', checkBeginEnd (yearBegin,yearEnd));
 
 if (yearBegin > yearEnd) {
     yearTemp = yearBegin;
@@ -86,11 +90,29 @@ if (yearBegin > yearEnd) {
     yearBegin = checkNum (yearBegin, "Начальный год", 1970);
     yearEnd = checkNum(yearEnd, "Конечный год", 2115);
 }
+//сортировка и вывод високосных
 for (let y = yearBegin; y <= yearEnd; y++){
   if ((y % 4 == 0)){
-    if((y % 100 == 0 && y % 400 == 0)){
+    if(!(y % 100 == 0 && y % 400 != 0)){
       console.log ('високосный ', y);
     } 
 
     }
   }
+}
+//   Задача №3
+// Написать программу, которая запрашивает у пользователя числа при помощи prompt, пока он не нажмет «Отмена». 
+// После нажатия «Отмена», программа выводит сумму введенных чисел. 
+// Если пользователь ввел не число (а, например, строку), то это значение не суммируется с остальными, а программа продолжает работать. 
+// Прерывается только в случае нажатия «Отмена».
+function task3(){
+  let num = 0,
+      inItem;
+  do {
+      inItem = prompt('Введите число', 1);
+      if (isNum(inItem)){
+        num+=+inItem;
+      }
+  } while (inItem !== null);
+  console.log (num);
+}
