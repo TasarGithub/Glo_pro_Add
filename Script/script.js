5bb40661c125330ba4ca4bc00a369b3d46643e5a
Taras Vyshegorodtsev <tasarvv@gmail.com>
2019-10-31 13:13:48 +0300

    plusZero done

--- Script/script.js
+++ Script/script.js
@@ -4,19 +4,18 @@
 
 // 2) Напишите функцию, которая будет добавлять 0 перед значениями которые состоят
 // из одной цифры (из 9:5:3  1.6.2019 сделает 09:05:03 01.06.2019)
-
-
-
-// 1) Выведите на страницу текущую дату и время в формате '09:59:59 30.05.2018'
-
-function getLocalMonth(n){
-  n++;
+function plusZero(n){
+  +n;
   n = (n < 10) ? '0'+ n : n + '';
   return n;
 }
 
-let now = new Date();
+// 1) Выведите на страницу текущую дату и время в формате '09:59:59 30.05.2018'
 
-let printNow = now.getHours()+':'+now.getMinutes()+':'+now.getSeconds()+' '+now.getDate()+'.'+getLocalMonth(now.getMonth())+'.'+now.getFullYear();
+let now = new Date();
+console.log('now: ', now + '');
+
+let printNow = plusZero(now.getHours()) + ':' + plusZero(now.getMinutes()) + ':' + plusZero(now.getSeconds()) + 
+' ' + plusZero(now.getDate()) + '.' + plusZero(now.getMonth() + 1) + '.' + now.getFullYear();
 console.log('printNow: ', printNow);
 document.write(printNow);
